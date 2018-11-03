a66cbc0c0447069bbc1dd8de5d6cbb38057214ad
ehcoo <garay.le8@gmail.com>
2018-11-03 17:00:50 +0000

    add data.js

---
+++ src/data/data.js
@@ -0,0 +1,50 @@
+export const DATABASE =[{
+    id: 0,
+    paper_name: 'Intellectual-Property Blockchain-Based Protection Model for Microfilms',
+    paper_author: 'Tsai, Wei-Tek and Feng, Libo and Zhang, Hui and You, Yue and Wang, Li and Zhong, Yao',
+    paper_date: '2017', // 这个日期格式要统一
+    paper_journal: 'IEEE',
+    paper_catalog: 'Blockchain',  // 如果多个数据，需要用 , 隔开， 分类不存在空格
+    paper_link: 'https://ieeexplore.ieee.org/document/7943309/',
+}, {
+    id: 1,
+    paper_name: 'A taxonomy of blockchain-based systems for architecture design',
+    paper_author: 'Xu, Xiwei and Weber, Ingo and Staples, Mark and Zhu, Liming and Bosch, Jan and Bass, Len and Pautasso, Cesare and Rimba, Paul',
+    paper_date: '2017', // 这个日期格式要统一
+    paper_journal: 'IEEE',
+    paper_catalog: 'Blockchain',  // 如果多个数据，需要用 , 隔开
+    paper_link: 'https://ieeexplore.ieee.org/document/7943309/',
+}, {
+    id: 2,
+    paper_name: 'Double-spending fast payments in bitcoin',
+    paper_author: 'Karame, Ghassan O and Androulaki, Elli and Capkun, Srdjan',
+    paper_date: '2012', // 这个日期格式要统一
+    paper_journal: 'ACM',
+    paper_catalog: 'Bitcoin,Blockchain',  // 如果多个数据，需要用 , 隔开
+    paper_link: 'https://ieeexplore.ieee.org/document/7943309/',
+},{
+    id: 3,
+    paper_name: 'Double-spending fast payments in bitcoin',
+    paper_author: 'Karame, Ghassan O and Androulaki, Elli and Capkun, Srdjan',
+    paper_date: '2015', // 这个日期格式要统一
+    paper_journal: 'ACM',
+    paper_catalog: 'Bitcoin,ABC',  // 如果多个数据，需要用 , 隔开
+    paper_link: 'https://ieeexplore.ieee.org/document/7943309/',
+},{
+    id: 4,
+    paper_name: 'Payments in bitcoin',
+    paper_author: 'Srdjan',
+    paper_date: '2012', // 这个日期格式要统一
+    paper_journal: 'ACM',
+    paper_catalog: 'Blockchain,ABC',  // 如果多个数据，需要用 , 隔开
+    paper_link: 'https://ieeexplore.ieee.org/document/7943309/',
+},{
+    id: 5,
+    paper_name: 'Slow payments in bitcoin',
+    paper_author: 'Elli and Capkun, Srdjan',
+    paper_date: '2000', // 这个日期格式要统一
+    paper_journal: 'ACM',
+    paper_catalog: 'ABC',  // 如果多个数据，需要用 , 隔开
+    paper_link: 'https://ieeexplore.ieee.org/document/7943309/',
+},
+];
